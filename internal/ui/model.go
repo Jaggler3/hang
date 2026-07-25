8baa171517cc72709e978245334b63389c0d3229
Martin D. <martin.protostar@gmail.com>
2026-07-25 02:06:14 -0400

    feat: add UI screen types and title screen with head selection
    - Add ScreenType enum (ScreenStart, ScreenGame)
    - Add head character options array
    - Add StartScreen factory with text input for player name

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -1,1 +1,32 @@
 package ui
+
+import "charm.land/bubbles/v2/textinput"
+
+type ScreenType int
+
+const (
+	ScreenStart ScreenType = iota
+	ScreenGame
+)
+
+type Screen struct {
+	ScreenType ScreenType
+	Elements   []any
+	HeadIndex  int
+}
+
+var HeadOptions = []string{"@", "o", "O", "X", "#", "&", "%", "$", "♥", "★", "♪", "0", "A", "M", "Z"}
+
+func NewStartScreen() Screen {
+	ti := textinput.New()
+	ti.Placeholder = "your name"
+	ti.Focus()
+	ti.CharLimit = 20
+	ti.SetWidth(20)
+
+	return Screen{
+		ScreenType: ScreenStart,
+		Elements:   []any{ti},
+		HeadIndex:  0,
+	}
+}
