3bb3dc4fe1cac3f236f888db1d54ea1161a2ee73
Martin D. <martin.protostar@gmail.com>
2026-07-25 02:06:32 -0400

    feat: wire world, data persistence, and title screen into main
    - Create default world on startup with LoadWorld
    - Add 30-second auto-save goroutine
    - Use NewGameModel handler with player and world references

--- cmd/hang/main.go
+++ cmd/hang/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
-	render "hang.sh/internal/render"
+	tea "charm.land/bubbletea/v2"
+	"github.com/charmbracelet/ssh"
+	"hang.sh/internal/data"
+	"hang.sh/internal/entity"
+	"hang.sh/internal/render"
 	"hang.sh/internal/server"
+	"hang.sh/internal/world"
 )
 
 func main() {
@@ -16,15 +22,33 @@ func main() {
 }
 
 func run() error {
-	srv, err := server.New(render.TeaHandler)
+	w := world.NewDefaultWorld()
+
+	if err := data.LoadWorld(w); err != nil {
+		fmt.Fprintf(os.Stderr, "warning: could not load world: %v\n", err)
+	}
+
+	go func() {
+		for {
+			time.Sleep(30 * time.Second)
+			if err := data.SaveWorld(w); err != nil {
+				fmt.Fprintf(os.Stderr, "warning: could not save world: %v\n", err)
+			}
+		}
+	}()
+
+	handler := func(sess ssh.Session) (tea.Model, []tea.ProgramOption) {
+		ctx := sess.Context()
+		p := ctx.Value(data.PlayerKey).(*entity.Player)
+		ctx.SetValue(data.WorldKey, w)
+		m := render.NewGameModel(p, w)
+		return m, nil
+	}
+
+	srv, err := server.New(handler)
 	if err != nil {
 		return err
 	}
 
-	err = srv.ListenAndServe()
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return srv.ListenAndServe()
 }
